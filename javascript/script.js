a0669065e7c30abbd0c1ec372a385a10b11e8769
Helen-Kuo <helenkuo85@gmail.com>
2025-12-04 16:26:56 +0800

    rwd微調

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 // ===== nav高度縮放 ====
 window.onscroll = () => {
     const navBar = document.querySelector(".nav-bar");
-    if(document.documentElement.scrollTop > 180){
+    if(document.documentElement.scrollTop > 50){
         navBar.classList.add("scrolled");
     }else{
         navBar.classList.remove("scrolled");
@@ -10,17 +10,90 @@ window.onscroll = () => {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    
+    const bodyElement = document.body;
     //====hb選單開關====//
     const navBar = document.querySelector('.nav-bar');
     const hbBtn = document.querySelector('.hb');
-    const overlay = document.querySelector('.mobile-menu-overlay')
-
-    hbBtn.addEventListener('click', () => {
+    const menuOverlay = document.querySelector('.mobile-menu-overlay')
+    
+    function toggleMenu(){
         navBar.classList.toggle('menu-open');
-    });
-    overlay.addEventListener('click', () => {
+        bodyElement.classList.toggle('open-fixed');
+    }
+    hbBtn.addEventListener('click', toggleMenu);
+
+    menuOverlay.addEventListener('click', () => {
         navBar.classList.remove('menu-open');
+        bodyElement.classList.remove('open-fixed');
+    });
+
+    // ==== modal圖片顯示 ==== //
+    const signupImg = document.querySelector('#signup-panel img');
+    const loginImg = document.querySelector('#login-panel img');
+    const switchLogin = document.querySelector('#switch-login a');
+    const switchSignUp = document.querySelector('#switch-signup a');
+    
+
+    switchLogin.addEventListener('click', (e) =>{
+        e.preventDefault();
+        signupImg.classList.add('imgHidden');
+        loginImg.classList.remove('imgHidden');
+    });
+    switchSignUp.addEventListener('click', (e) => {
+        e.preventDefault();
+        loginImg.classList.add('imgHidden');
+        signupImg.classList.remove('imgHidden');
+    });
+    
+
+    // ==== 跳窗功能 ==== //
+    const loginModal = document.querySelector('#account');
+    const accountBtn = document.querySelector('.account-btn');
+    const closeBtns = document.querySelectorAll('.close-btn');
+    const tourGuideModal = document.querySelector('#tour-guide');
+    const tourGuideBtn = document.querySelectorAll('.tour-guide-btn');
+    const modalOverlay = document.querySelector('.modal-overlay');
+
+    if(accountBtn){
+        accountBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log("click");
+            loginModal.classList.remove('modalHidden');
+            bodyElement.classList.add('open-fixed');
+        });
+    }
+    if(tourGuideBtn){
+        tourGuideBtn.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                tourGuideModal.classList.remove('modalHidden');
+                bodyElement.classList.add('open-fixed');
+            });
+        });
+    }
+    closeBtns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(btn.closest('#tour-guide')){
+                tourGuideModal.classList.add('modalHidden');
+                bodyElement.classList.remove('open-fixed');
+            }else if(btn.closest('#account')){
+                loginModal.classList.add('modalHidden');
+                bodyElement.classList.remove('open-fixed');
+            }
+        });
+    });
+
+    //==== font-scale 開闔 ====//
+    const toggleButton = document.querySelector('.font-scale div');
+    const fontScaleContainer = document.querySelector('.font-scale');
+    const fontScaleButtons = document.querySelector('.font-scale-buttons');
+    
+    toggleButton.addEventListener('click', () => {
+        fontScaleContainer.classList.toggle('open');
+    });
+    fontScaleButtons.addEventListener('mouseleave', () => {
+        fontScaleContainer.classList.remove('open');
     });
 
 
@@ -60,18 +133,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     //swiper3.best-moments-swiper
-    // const bestMomentsSwiper = new Swiper('.best-moments-swiper', {
-    //     slidesPerView: 'auto',
-    //     spaceBetween: 0,
-    //     loop: true,
-    //     loopedSlides: 8,
-    //     allowTouchMove: false,
-    //     autoplay: {
-    //         delay: 0,
-    //         disableOnInteraction: false,
-    //     },
-    //     speed: 3000,
-    // });
+    const bestMomentsSwiper = new Swiper('.best-moments-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: -1,
+        loop: true,
+        loopedSlides: 8,
+        allowTouchMove: false,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+        speed: 3000,
+    });
 
     //rwd indoor-img swiper
     const indoorImgSwiper = new Swiper('#indoor-img-swiper', {
@@ -173,73 +246,5 @@ document.addEventListener('DOMContentLoaded', function() {
             button.classList.add('active');
             
         })
-    });
-
-    // ==== modal圖片顯示 ==== //
-    const signupImg = document.querySelector('#signup-panel img');
-    const loginImg = document.querySelector('#login-panel img');
-    const switchLogin = document.querySelector('#switch-login a');
-    const switchSignUp = document.querySelector('#switch-signup a');
-    switchLogin.addEventListener('click', (e) =>{
-        e.preventDefault();
-        signupImg.classList.add('imgHidden');
-        loginImg.classList.remove('imgHidden');
-    });
-    switchSignUp.addEventListener('click', (e) => {
-        e.preventDefault();
-        loginImg.classList.add('imgHidden');
-        signupImg.classList.remove('imgHidden');
-    });
-
-    // ==== 跳窗功能 ==== //
-    const bodyElement = document.body;
-    const loginModal = document.querySelector('#account');
-    const accountBtn = document.querySelector('.account-btn');
-    const closeBtns = document.querySelectorAll('.close-btn');
-    const tourGuideModal = document.querySelector('#tour-guide');
-    const tourGuideBtn = document.querySelectorAll('.tour-guide-btn');
-
-
-    if(accountBtn){
-        accountBtn.addEventListener('click', (e) => {
-            e.preventDefault();
-            console.log("click");
-            loginModal.classList.remove('modalHidden');
-            bodyElement.style.overflow = 'hidden';
-        });
-    }
-    if(tourGuideBtn){
-        tourGuideBtn.forEach(btn => {
-            btn.addEventListener('click', (e) => {
-                e.preventDefault();
-                tourGuideModal.classList.remove('modalHidden');
-                bodyElement.style.overflow = 'hidden';
-            });
-        });
-        
-    }
-    closeBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-            if(btn.closest('#tour-guide')){
-                tourGuideModal.classList.add('modalHidden');
-                bodyElement.style.overflow = 'auto';
-            }else if(btn.closest('#account')){
-                loginModal.classList.add('modalHidden');
-                bodyElement.style.overflow = 'auto';
-            }
-        });
-    });
-
-    //==== font-scale 開闔 ====//
-    const toggleButton = document.querySelector('.font-scale div');
-    const fontScaleContainer = document.querySelector('.font-scale');
-    const fontScaleButtons = document.querySelector('.font-scale-buttons');
-    
-    toggleButton.addEventListener('click', () => {
-        fontScaleContainer.classList.toggle('open');
-    });
-    fontScaleButtons.addEventListener('mouseleave', () => {
-        fontScaleContainer.classList.remove('open');
     });
 });
